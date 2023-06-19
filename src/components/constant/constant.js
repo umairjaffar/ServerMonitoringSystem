22a32e9aa714e0502d396c127e56c2f4fb9d6062
Umair Jaffar <umairjaffar@knowpakistan.org>
2023-06-19 11:52:33 -0700

    api url changes

--- src/components/constant/constant.js
+++ src/components/constant/constant.js
@@ -1,5 +1,5 @@
 // base url for all api's
-export const BASE_URL = "https://bc34-154-192-15-201.ngrok-free.app";
+export const BASE_URL = "https://8ad4-182-191-88-177.ngrok-free.app";
 // access token for header.
 export const accesstoken = localStorage.getItem("access_token");
 export const config = {
